56b25f2b578721d66024b5d9acf5936f5b23f8fc
sgigli <simon.luciano@gigli.net>
2020-07-21 09:16:04 -0400

    Completed indecision app

--- src/app.js
+++ src/app.js
@@ -4,14 +4,4 @@ import IndecisionApp from './components/IndecisionApp'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>header</p>
-            {props.children}
-            <p>footer</p>
-        </div>
-    )
-}
-
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
